5a7f3da32eb971a2f93ca23e64b5aff4e9afbc2e
HalisBay <eminbay_58@hotmail.com>
2021-09-03 15:49:09 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInValid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "Ürün listelendi";
+        public static string ProductCountOfCategoryError = "En fazla 15 kategori olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde bir ürün daha bulunmaktadır.Ürün ismini değiştiriniz";
+        public static string CategoryLimitExceded = "Kategori limiti 15 den yükske olamaz, limit aşıldı eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "parola hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Sisteme Giriş Yapıldı";
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ProductId = 1,CategoryId = 1,ProductName = "Çikolata(500g)", UnitPrice = 15,UnitsInStock = 15},
+                new Product{ProductId = 2,CategoryId = 1,ProductName = "çerez", UnitPrice = 500,UnitsInStock = 3},
+                new Product{ProductId = 3,CategoryId = 2,ProductName = "batarya", UnitPrice = 1500,UnitsInStock = 2},
+                new Product{ProductId = 4,CategoryId = 2,ProductName = "kamera", UnitPrice = 150,UnitsInStock = 65},
+                new Product{ProductId = 5,CategoryId = 2,ProductName = "maus", UnitPrice = 85,UnitsInStock = 1}
+            };
+        }
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.FirstOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public void Delete(Product product)
+        {
+
+            Product productToDelete = _products.FirstOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAllByCategory(int CategoryId)
+        {
+            return _products.Where(p => p.CategoryId == CategoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
